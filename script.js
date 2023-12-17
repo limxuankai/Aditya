29801ca1f2d50069823cf1bfc7093283db32fb68
limxuankai <limxuankai3645@gmail.com>
2023-12-17 18:27:47 +0800

    Done

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-var i =0
-const Order = ['aboutme','skills','project','test','contact']
 function revealContent() {
-    if (window.getComputedStyle(document.getElementById('landing-page')).display == "block"){
-        document.getElementById('landing-page').style.display = 'none';
-        document.getElementById('aboutme').style.display = 'block';
-    }
-    else{
-        if (i <Order.length-1){
-        i = i + 1
-        document.getElementById(Order[i]).style.display ='block'
+    var landingPage = document.getElementById('landing-page');
+
+    if (landingPage && window.getComputedStyle(landingPage).display === 'block') {
+        landingPage.style.display = 'none';
+
+        var elementsWithClassK = document.getElementsByClassName('k');
+        for (var i = 0; i < elementsWithClassK.length; i++) {
+            elementsWithClassK[i].style.display = 'block';
         }
     }
-  }
+}
